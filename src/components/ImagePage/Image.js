18c49b807789da7906ecd1caad0f8e528f2c33b3
trung <trungdovnn3@gmail.com>
2022-03-29 21:19:01 +0700

    Edit Page Image

--- src/components/ImagePage/Image.js
+++ src/components/ImagePage/Image.js
@@ -62,7 +62,7 @@ class Image extends React.Component {
                                                 width="50px"
                                                 height="50px"
                                                 fill="currentColor"
-                                                class="bi bi-valentine"
+                                                className="bi bi-valentine"
                                                 viewBox="0 0 16 16"
                                             >
                                                 <path d="M8 5.993c1.664-1.711 5.825 1.283 0 5.132-5.825-3.85-1.664-6.843 0-5.132ZM2.25 4a.25.25 0 0 0-.25.25v1.5a.25.25 0 0 0 .5 0V4.5h1.25a.25.25 0 0 0 0-.5h-1.5Zm10 0a.25.25 0 1 0 0 .5h1.25v1.25a.25.25 0 1 0 .5 0v-1.5a.25.25 0 0 0-.25-.25h-1.5ZM2.5 10.25a.25.25 0 1 0-.5 0v1.5c0 .138.112.25.25.25h1.5a.25.25 0 1 0 0-.5H2.5v-1.25Zm11.5 0a.25.25 0 1 0-.5 0v1.25h-1.25a.25.25 0 1 0 0 .5h1.5a.25.25 0 0 0 .25-.25v-1.5Z" />
@@ -78,43 +78,163 @@ class Image extends React.Component {
                             <from>
                                 <div className="form-group mt-2 text-center">
                                     <button className="btn btn-primary m-1">
-                                        <i class="bi bi-upload mx-1"></i>
+                                        <i className="bi bi-upload mx-1"></i>
                                         Tải lên
                                     </button>
                                     <button className="btn btn-primary m-1">
-                                        <i class="bi bi-lock-fill mx-1"></i>
+                                        <i className="bi bi-lock-fill mx-1"></i>
                                         Bảo mật
                                     </button>
                                     <button className="btn btn-primary m-1">
-                                        <i class="bi bi-share-fill mx-1"></i>
+                                        <i className="bi bi-share-fill mx-1"></i>
                                         Shared
                                     </button>
                                 </div>
                             </from>
-                            <section className="works clearfix">
-                                <div className="project-wrapper">
-                                    <figure className="">
-                                        <img
-                                            src="https://i.pinimg.com/originals/c9/07/2d/c9072dd25a88a06ef2d13da266e59fa9.jpg"
-                                            alt="Girl in a jacket"
-                                            width="350"
-                                            height="400"
-                                        />
-                                        <figcaption class="overlay">
-                                            <a
-                                                class="fancybox"
-                                                rel="works"
-                                                title="Write Your Image Caption Here"
-                                                href="https://i.pinimg.com/originals/c9/07/2d/c9072dd25a88a06ef2d13da266e59fa9.jpg"
-                                            >
-                                                <i class="fa fa-eye fa-lg"></i>
-                                            </a>
-                                            <h4>Labore et dolore magnam</h4>
-                                            <p>Photography</p>
-                                        </figcaption>
-                                    </figure>
-                                </div>
-                            </section>
+                            <div className="project-wrapper">
+                                <figure className="mix work-item branding">
+                                    <img
+                                        src="https://cellphones.com.vn/sforum/wp-content/uploads/2021/11/2-20.jpg"
+                                        alt=""
+                                    />
+                                    <figcaption className="overlay">
+                                        <a
+                                            className="fancybox"
+                                            rel="works"
+                                            title="Write Your Image Caption Here"
+                                            href="https://cellphones.com.vn/sforum/wp-content/uploads/2021/11/2-20.jpg"
+                                        >
+                                            <i className="fa fa-eye fa-lg"></i>
+                                        </a>
+                                        <h4>Labore et dolore magnam</h4>
+                                        <p>Photography</p>
+                                    </figcaption>
+                                </figure>
+
+                                <figure className="mix work-item web">
+                                    <img
+                                        src="https://cellphones.com.vn/sforum/wp-content/uploads/2021/11/2-20.jpg"
+                                        alt=""
+                                    />
+                                    <figcaption className="overlay">
+                                        <a
+                                            className="fancybox"
+                                            rel="works"
+                                            title="Write Your Image Caption Here"
+                                            href="https://cellphones.com.vn/sforum/wp-content/uploads/2021/11/2-20.jpg"
+                                        >
+                                            <i className="fa fa-eye fa-lg"></i>
+                                        </a>
+                                        <h4>Labore et dolore magnam</h4>
+                                        <p>Photography</p>
+                                    </figcaption>
+                                </figure>
+
+                                <figure className="mix work-item logo-design">
+                                    <img
+                                        src="https://cellphones.com.vn/sforum/wp-content/uploads/2021/11/2-20.jpg"
+                                        alt=""
+                                    />
+                                    <figcaption className="overlay">
+                                        <a
+                                            className="fancybox"
+                                            rel="works"
+                                            title="Write Your Image Caption Here"
+                                            href="https://cellphones.com.vn/sforum/wp-content/uploads/2021/11/2-20.jpg"
+                                        >
+                                            <i className="fa fa-eye fa-lg"></i>
+                                        </a>
+                                        <h4>Labore et dolore magnam</h4>
+                                        <p>Photography</p>
+                                    </figcaption>
+                                </figure>
+
+                                <figure className="mix work-item photography">
+                                    <img
+                                        src="https://cellphones.com.vn/sforum/wp-content/uploads/2021/11/2-20.jpg"
+                                        alt=""
+                                    />
+                                    <figcaption className="overlay">
+                                        <a
+                                            className="fancybox"
+                                            rel="works"
+                                            title="Write Your Image Caption Here"
+                                            href="https://cellphones.com.vn/sforum/wp-content/uploads/2021/11/2-20.jpg"
+                                        >
+                                            <i className="fa fa-eye fa-lg"></i>
+                                        </a>
+                                        <h4>Labore et dolore magnam</h4>
+                                        <p>Photography</p>
+                                    </figcaption>
+                                </figure>
+
+                                <figure className="mix work-item branding">
+                                    <img
+                                        src="https://cellphones.com.vn/sforum/wp-content/uploads/2021/11/2-20.jpg"
+                                        alt=""
+                                    />
+                                    <figcaption className="overlay">
+                                        <a
+                                            className="fancybox"
+                                            rel="works"
+                                            title="Write Your Image Caption Here"
+                                            href="https://cellphones.com.vn/sforum/wp-content/uploads/2021/11/2-20.jpg"
+                                        >
+                                            <i className="fa fa-eye fa-lg"></i>
+                                        </a>
+                                        <h4>Labore et dolore magnam</h4>
+                                        <p>Photography</p>
+                                    </figcaption>
+                                </figure>
+
+                                <figure className="mix work-item web">
+                                    <img src="img/works/item-6.jpg" alt="" />
+                                    <figcaption className="overlay">
+                                        <a
+                                            className="fancybox"
+                                            rel="works"
+                                            title="Write Your Image Caption Here"
+                                            href="img/works/item-6.jpg"
+                                        >
+                                            <i className="fa fa-eye fa-lg"></i>
+                                        </a>
+                                        <h4>Labore et dolore magnam</h4>
+                                        <p>Photography</p>
+                                    </figcaption>
+                                </figure>
+
+                                <figure className="mix work-item logo-design">
+                                    <img src="img/works/item-7.jpg" alt="" />
+                                    <figcaption className="overlay">
+                                        <a
+                                            className="fancybox"
+                                            rel="works"
+                                            title="Write Your Image Caption Here"
+                                            href="img/works/item-7.jpg"
+                                        >
+                                            <i className="fa fa-eye fa-lg"></i>
+                                        </a>
+                                        <h4>Labore et dolore magnam</h4>
+                                        <p>Photography</p>
+                                    </figcaption>
+                                </figure>
+
+                                <figure className="mix work-item photography">
+                                    <img src="img/works/item-8.jpg" alt="" />
+                                    <figcaption className="overlay">
+                                        <a
+                                            className="fancybox"
+                                            rel="works"
+                                            title="Write Your Image Caption Here"
+                                            href="img/works/item-8.jpg"
+                                        >
+                                            <i className="fa fa-eye fa-lg"></i>
+                                        </a>
+                                        <h4>Labore et dolore magnam</h4>
+                                        <p>Photography</p>
+                                    </figcaption>
+                                </figure>
+                            </div>
                         </div>
                     </>
                 )}
