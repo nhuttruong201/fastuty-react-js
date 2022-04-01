00f22ad4477e7d5c223db82de421295b47e8455a
trung <trungdovnn3@gmail.com>
2022-04-01 13:42:15 +0700

    css page image

--- src/components/ImagePage/Image.js
+++ src/components/ImagePage/Image.js
@@ -8,7 +8,12 @@ class Image extends React.Component {
         super(props);
         this.state = {
             isLoading: true,
-            images: [],
+            images: [
+                {
+                    _id: "",
+                    title: "",
+                },
+            ],
         };
     }
     fetchData = async (imageCode) => {
@@ -35,6 +40,12 @@ class Image extends React.Component {
         // }, 2000);
         await this.fetchData(imageCode);
     }
+
+    updateTitle = async (title) => {
+        let res = await axios.get(`/api/images/${title}`);
+        let { images } = res.data;
+        this.setState({ images: images, title: title });
+    };
 
     render() {
         let { isLoading, images } = this.state;
@@ -75,22 +86,20 @@ class Image extends React.Component {
                                     </div>
                                 </div>
                             </section>
-                            <form>
-                                <div className="form-group mt-2 text-center">
-                                    <button className="btn btn-primary m-1">
-                                        <i className="bi bi-upload mx-1"></i>
-                                        Tải lên
-                                    </button>
-                                    <button className="btn btn-primary m-1">
-                                        <i className="bi bi-lock-fill mx-1"></i>
-                                        Bảo mật
-                                    </button>
-                                    <button className="btn btn-primary m-1">
-                                        <i className="bi bi-share-fill mx-1"></i>
-                                        Shared
-                                    </button>
-                                </div>
-                            </form>
+                            <div className="form-group mt-2 text-center">
+                                <button className="btn btn-primary m-1">
+                                    <i className="bi bi-upload mx-1"></i>
+                                    Tải lên
+                                </button>
+                                <button className="btn btn-primary m-1">
+                                    <i className="bi bi-lock-fill mx-1"></i>
+                                    Bảo mật
+                                </button>
+                                <button className="btn btn-primary m-1">
+                                    <i className="bi bi-share-fill mx-1"></i>
+                                    Shared
+                                </button>
+                            </div>
                             <div className="project-wrapper">
                                 {images.map((item, index) => (
                                     <figure className="mix work-item branding">
@@ -111,11 +120,19 @@ class Image extends React.Component {
                                                             title={item.title}
                                                         >
                                                             <i className="fa fa-eye fa-lg"></i>
+                                                            <svg
+                                                                xmlns="http://www.w3.org/2000/svg"
+                                                                width="16"
+                                                                height="16"
+                                                                fill="currentColor"
+                                                                class="bi bi-trash3"
+                                                                viewBox="0 0 16 16"
+                                                            >
+                                                                <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z" />
+                                                            </svg>
                                                         </a>
                                                         <br></br>
-                                                        <span>
-                                                            {item.title}
-                                                        </span>
+                                                        <h1>{item.title}</h1>
                                                     </div>
                                                 ))}
                                             </>
